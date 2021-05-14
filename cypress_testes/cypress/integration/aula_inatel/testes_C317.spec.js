c1a9bc2d2c7c5c70ae1448e14a3136ce3743e40e
Chris Lima <christopher@inatel.br>
2021-05-14 20:44:41 -0300

    Arquivo exemplo para testes para a turma C317

---
+++ cypress_testes/cypress/integration/aula_inatel/testes_C317.spec.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress"/>
+
+describe('Caso de Teste: 1', () => {
+
+    it('Cenario de Teste: 1', () => { 
+
+        //TODO os seus comandos de teste
+        
+    });
+    it('Cenario de Teste: 2', () => { 
+        
+        //TODO os seus comandos de teste
+        
+    });
+    it('Cenario de Teste: 3', () => { 
+        
+        //TODO os seus comandos de teste
+        
+    });
+    it('Cenario de Teste: 4', () => { 
+        
+        //TODO os seus comandos de teste
+        
+    });
+    it('Cenario de Teste: 5', () => { 
+        
+        //TODO os seus comandos de teste
+        
+    });
+    it('Cenario de Teste: 6', () => { 
+        
+        //TODO os seus comandos de teste
+        
+    });
+
+});
